8478176089db0cd4dea7e9677c67c344312aa773
Jianmin <nicklee19950330@gmail.com>
2020-08-24 01:40:56 -0500

    spring security with oath2

---
+++ src/main/java/com/demo/springboot/SpringBootOath2Application.java
@@ -0,0 +1,13 @@
+package com.demo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOath2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOath2Application.class, args);
+	}
+
+}
